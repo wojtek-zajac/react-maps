7cd6dbf783db6539a30250bd2ababdd68e957697
Wojciech Zając <wojciechzajaaac@gmail.com>
2018-08-18 22:27:27 +0200

    Display map

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css'
 
 class App extends Component {
 
-initMa = () => {
-  const map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 50.06465, lng: 19.94498},
-    zoom: 8
-  })
-}
+  componentDidMount() {
+    this.renderMap()
+  }
 
+  renderMap = () => {
+    loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyDziy5R3lKj_zp1jOfiuH-TAncmOqG1MGo&v=3&callback=initMap')
+    window.initMap = this.initMap
+  }
+
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 50.06465, lng: 19.94498},
+      zoom: 13
+    })
+  }
 
   render() {
     return (
@@ -18,6 +26,15 @@ initMa = () => {
       </main>
     )
   }
+}
+
+function loadScript(url) {
+  const index = window.document.getElementsByTagName('script')[0]
+  const script = window.document.createElement('script')
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
 }
 
 export default App
