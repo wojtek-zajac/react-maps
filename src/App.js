ee3878d3470221161745a141b98c155b71cd216d
Wojciech Zając <wojciechzajaaac@gmail.com>
2018-08-19 22:49:33 +0200

    Filter makrers and display info window on click

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FilterVenues from './Components/FilterVenues'
 import Map from './Components/Map'
 import * as FoursquareAPI from './FoursquareAPI'
 
+let openInfoWindow
 
 class App extends Component {
 
@@ -15,16 +16,15 @@ class App extends Component {
     visibleVenues: [],
     markers: [],
     visibleInfoWindow: '',
-    isInfoVindowOpen: false
+    isInfoVindowOpen: false,
+    infoWindows: [],
   }
-
 
   updateVisibleVenues = (visibleVenues) => {
     this.setState({
       visibleVenues: visibleVenues
     })
   }
-
 
   componentDidMount() {
     this.getFousquareData()
@@ -44,8 +44,6 @@ class App extends Component {
     }) 
   }
 
-
-
   showInfoWindow = (venue) => {
     console.log(venue)
     this.setState({
@@ -54,67 +52,63 @@ class App extends Component {
     })
   }
 
-
   renderMap = () => {
     loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyDziy5R3lKj_zp1jOfiuH-TAncmOqG1MGo&v=3&callback=initMap')
     window.initMap = this.initMap
   }
 
   initMap = () => {
-
     // Create a map
     const map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 50.06465, lng: 19.94498},
       zoom: 13
     })
     console.log(map)
-
-    // Create an InfoWindow
-    const infoWindow = new window.google.maps.InfoWindow()
-
     
-
-// { 
-
     // Display Dynamic Markers
     this.state.visibleVenues.map(venue => {
      
       const venueName = `${venue.name}`
-
+      
       // Create a marker
       const marker = new window.google.maps.Marker({
         position: {lat: venue.location.lat, lng: venue.location.lng},
         map: map,
-        title: venue.name
+        title: `${venue.name} - click for more details`
       })
 
       this.state.markers.push(marker)
 
+      // Create an InfoWindow
+      const infoWindow = new window.google.maps.InfoWindow()
 
-      // Click on a marker
-      marker.addListener('click', function() {
-
-        // Change content
-        infoWindow.setContent(venueName)
-
-        // Open an InfoWindow
-        infoWindow.open(map, marker)
+      this.state.infoWindows.push(infoWindow)
+     
+      // On a marker click
+      marker.addListener('click', () => {
+        if (openInfoWindow) {
+          marker.open = false
+          openInfoWindow.close()
+        }
+        if (!marker.open) {
+            // Open an InfoWindow and set its content
+            marker.open = true
+            openInfoWindow = infoWindow
+            infoWindow.setContent(venueName)
+            infoWindow.open(map, marker)
+        // Close an InfoWindow
+        } else {
+          marker.open = false
+          infoWindow.close()
+        }
       })
     })
-
-  // }
-
-
-
-
-
   }
 
   render() {
     return (
       <div className="app">
         <Header/>
-
         <main>
           <FilterVenues
             venues={this.state.venues}
@@ -122,11 +116,10 @@ class App extends Component {
             updateVisibleVenues={this.updateVisibleVenues}
             showInfoWindow={this.showInfoWindow}
             markers={this.state.markers}
+            infoWindows={this.state.infoWindows}
           />
-
           <Map/>
         </main>
-
         <Footer/>
       </div>
     )
--- src/Components/FilterVenues.js
+++ src/Components/FilterVenues.js
@@ -4,53 +4,81 @@ import sortBy from 'sort-by'
 
 class FilterVenues extends Component {
 
-    onFilterChange(query) {
-        let showingNames
-        if(query) {
-            const match = new RegExp(escapeRegExp(query), 'i')
-            showingNames = this.props.venues.filter((venue) => match.test(venue.name))
+  onFilterChange(query ) {
 
-            // this.props.updateVisibleVenues(showingNames)
+    const allVenues = this.props.venues
+    const visibleVenues = this.props.visibleVenues
+    const map = document.getElementById('map')
+    const markers = this.props.markers
+    const infoWindows = this.props.infoWindows
+    let showingNames
+
+    
+
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), 'i')
+      showingNames = allVenues.filter((venue) => match.test(venue.name))
+    } else {
+      showingNames = allVenues
+    }
+       
+    showingNames.sort(sortBy('name'))
+
+    this.props.updateVisibleVenues(showingNames)
+    
+    // Dynamic update markers on the map - Udacity webinar
+    if (query !== '') {
+      allVenues.forEach((venue, index) => {
+        if (venue.name.toLowerCase().includes(query.toLowerCase())) {
+          markers[index].setVisible(true)
         } else {
-            showingNames = this.props.venues
+          if (visibleVenues.marker === markers[index]) {
+            visibleVenues.close()
+          }
+          markers[index].setVisible(false)
+          infoWindows[index].close(map, markers[index]);
         }
+      })
+    } else {
+      allVenues.forEach((venue, index) => {
+        if (markers.length && markers[index]) {
+          markers[index].setVisible(true)
+          markers[index].setAnimation(window.google.maps.Animation.DROP);
+          infoWindows[index].close(map, markers[index]);
+        }
+      })
+    }
+    console.log(this.props.visibleVenues,'Visible Venues')
+  }
+
+  render() {
+    return(
+      <aside className="filter-container">
         
-        showingNames.sort(sortBy('name'))
-
-        this.props.updateVisibleVenues(showingNames)
-    }
-
-
-    render() {
-        return(
-            <aside className="filter-container">
-                
-                <input
-                    className="filter-input"
-                    type="text"
-                    placeholder="Filter restaurants"
-                    value={this.showingNames}
-                    onChange={(event) => this.onFilterChange(event.target.value)}
+        <input
+          className="filter-input"
+          type="text"
+          placeholder="Filter restaurants"
+          value={this.showingNames}
+          onChange={(event) => this.onFilterChange(event.target.value)}
+        >
+        </input>
+        <ul className="list-items">
+          {this.props.visibleVenues.map(venue => {
+              return (
+                <li 
+                  key={venue.id}
+                  onClick={() => {this.props.showInfoWindow(venue.id)}}
                 >
-                </input>
-
-                <ul className="list-items">
-                    {this.props.visibleVenues.map(venue => {
-                            return (
-                                <li 
-                                    key={venue.id}
-                                    onClick={() => {this.props.showInfoWindow(venue.id)}}
-                                >
-                                    {venue.name}
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-
-            </aside>
-        )
-    }
+                  {venue.name}
+                </li>
+              )
+            })
+          }
+        </ul>
+      </aside>
+    )
+  }
 }
 
 export default FilterVenues
